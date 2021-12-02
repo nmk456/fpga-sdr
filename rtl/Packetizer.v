f768641d475a88a479664d3e072a09b976a4cbb6
Nathan Kerns <nmk456@gmail.com>
2021-12-01 21:59:47 -0500

    Finished (?) Packetizer

--- rtl/Packetizer.v
+++ rtl/Packetizer.v
@@ -31,19 +31,29 @@ module Packetizer (
     parameter SOURCE_PORT = 16'd32179;
     parameter DEST_PORT = 16'd32179;
 
+    localparam NUM_SAMPLES = 366; // Number of IQ samples
+    localparam IQ_LEN = NUM_SAMPLES * 4; // Length of IQ data in bytes
+    localparam DATA_LEN = IQ_LEN + 8; // Length of UDP payload in bytes
+    localparam UDP_LEN = DATA_LEN + 8; // Length of UDP packet in bytes
+    localparam IP_LEN = UDP_LEN + 20; // Length of IP packet in bytes
+    localparam PACKET_LEN = IP_LEN + 14; // Length of entire packet in bytes
+
     // IQ Data
 
-    reg[31:0] IQdata = 0;
-    wire[15:0] next_I = IQdata[31:16];
-    wire[15:0] next_Q = IQdata[15:0];
-    reg IQready = 0;
+    reg[31:0] iq_buffer = 0, iq_buffer_p1 = 0;
+    wire[15:0] next_I = {{3{iq_buffer[29]}}, iq_buffer[29:17]};
+    wire[15:0] next_Q = {{3{iq_buffer[13]}}, iq_buffer[13:1]};
+    // reg IQready = 0;
+    // reg first_run = 0;
+    reg new_sample = 0, new_sample_last = 0;
+    reg waited = 0;
 
     // Ethernet
 
     reg[15:0] tx_word = 0;
     reg[63:0] packet_counter = 0;
 
-    reg[7:0] wait_counter = 0;
+    // reg[7:0] wait_counter = 0;
 
     // TODO: Checksums
     reg[15:0] ip_checksum = 0;
@@ -68,8 +78,10 @@ module Packetizer (
             16'h000d: tx_tdata = 8'h00;
             16'h000e: tx_tdata = 8'h45;                 // IPv4
             16'h000f: tx_tdata = 8'h00;                 // DSCP/ECN 
-            16'h0010: tx_tdata = 8'h05;                 // IP length
-            16'h0011: tx_tdata = 8'hdc;
+            // 16'h0010: tx_tdata = 8'h05;                 // IP length
+            // 16'h0011: tx_tdata = 8'hdc;
+            16'h0010: tx_tdata = IP_LEN[15:8];          // IP length
+            16'h0011: tx_tdata = IP_LEN[7:0];
             16'h0012: tx_tdata = packet_counter[15:8];  // Packet ID
             16'h0013: tx_tdata = packet_counter[7:0];
             16'h0014: tx_tdata = 8'h00;                 // Fragment offset
@@ -90,8 +102,10 @@ module Packetizer (
             16'h0023: tx_tdata = SOURCE_PORT[7:0];
             16'h0024: tx_tdata = DEST_PORT[15:8];       // DEstination port
             16'h0025: tx_tdata = DEST_PORT[7:0];
-            16'h0026: tx_tdata = 8'h05;                 // UDP length
-            16'h0027: tx_tdata = 8'hc8;
+            // 16'h0026: tx_tdata = 8'h05;                 // UDP length
+            // 16'h0027: tx_tdata = 8'hc8;
+            16'h0026: tx_tdata = UDP_LEN[15:8];         // UDP length
+            16'h0027: tx_tdata = UDP_LEN[7:0];
             16'h0028: tx_tdata = udp_checksum[15:8];    // UDP checksum
             16'h0029: tx_tdata = udp_checksum[7:0];
             16'h002a: tx_tdata = packet_counter[7:0];   // Packet ID for GNU Radio
@@ -117,57 +131,148 @@ module Packetizer (
     always @(posedge clk) begin
         if (rst) begin
             tx_word <= 0;
-            // packet_counter <= 0;
+            packet_counter <= 0;
+            lvds_tready <= 0;
+            tx_tvalid <= 0;
+            waited <= 0;
 
             // Cancel current frame
-            tx_tuser <= 1;
-            tx_tlast <= 1;
+            // tx_tuser <= 1;
+            // tx_tlast <= 1;
         end else begin
-            lvds_tready <= 0;
+//             tx_tvalid <= 1;
 
-            if (wait_counter > 0) begin
-                wait_counter <= wait_counter - 1;
-            end else begin
-                if (tx_tuser) begin
-                    tx_tuser <= 0;
+//             if (tx_tuser) begin
+//                 tx_tuser <= 0;
+//                 tx_tlast <= 0;
+//             end
+
+//             if (lvds_tready && lvds_tvalid && ((tx_word != 16'h0033) || packet_counter != 64'b0)) begin
+//                 lvds_tready <= 0;
+//                 iq_buffer <= lvds_tdata;
+//                 new_sample <= 1;
+//             end
+
+//             if (tx_tready) begin
+//                 // Either increment tx_word or set tx_tvalid to low
+//                 if (~new_sample && (tx_word[1:0] == 2'b01) &&
+//                 (tx_word > 16'h0031) && (tx_word != 16'h05e9))
+//                     tx_tvalid <= 0;
+//                 else
+//                     tx_word <= tx_word + 1;
+//             end
+
+//             // if (new_sample) begin
+//             //     new_sample <= 0;
+//             // end
+// //             if (new_sample && (tx_word[1:0] == 2'b01) && (tx_word > 16'h0031) && (tx_word != 16'h05e9)) begin
+// //                 new_sample <= 0;
+// // iq_buffer <= lvds_tdata;
+// //             end
+
+//             case (tx_word)
+//                 16'h0001: begin
+//                     // Prime iq_buffer at startup
+//                     // if (~lvds_tready) begin
+//                     //     lvds_tready <= 1;
+//                     // end
+//                 end
+//                 // 16'h0003: iq_buffer <= lvds_tdata;
+//                 default: begin
+//                     if ((tx_word >= 16'h0031)) begin
+//                         // iq_buffer <= lvds_tdata;
+//                         // lvds_tready <= 1;
+//                         // new_sample <= 0;
+//                         if ((tx_word[1:0] == 2'b01) && new_sample && tx_tready) begin
+//                             // iq_buffer <= lvds_tdata;
+//                             // lvds_tready <= 1;
+//                             new_sample <= 0;
+//                         end else if ((tx_word[1:0] == 2'b10) && ~new_sample && ~lvds_tready) begin
+//                             lvds_tready <= 1;
+//                         end
+//                     end
+
+//                     if ((tx_word == 16'h05e8) && tx_tready) begin
+//                         tx_tlast <= 1;
+//                     end
+
+//                 end
+//                 16'h05e9: begin // Last
+//                     if (tx_tready) begin
+//                         // IQready <= 0;
+//                         tx_tlast <= 0;
+//                         // tx_tvalid <= 0; // Leaving this commented out might cause issues
+//                         tx_word <= 0;
+//                         packet_counter <= packet_counter + 1;
+//                         // wait_counter <= 16;
+//                         // first_run <= 0;
+//                     end
+//                 end
+//             endcase
+
+            tx_tvalid <= 1;
+            new_sample_last <= new_sample;
+
+            // Either increment tx_word or set tx_tvalid to low
+            if (tx_tready && tx_tvalid) begin
+                tx_word <= tx_word + 1;
+
+                // Prime buffer on first run
+                if ((packet_counter == 64'b0) && (tx_word == 16'h002e)) begin
+                    lvds_tready <= 1;
+                end
+            end
+
+            if (lvds_tready && lvds_tvalid) begin
+                iq_buffer_p1 <= lvds_tdata;
+                lvds_tready <= 0;
+                new_sample <= 1;
+            end
+
+            // Finalize packet
+            if (tx_word == 16'h05e9) begin
+                if (tx_tready) begin
                     tx_tlast <= 0;
+                    tx_word <= 0;
+                    packet_counter <= packet_counter + 1;
+                end
+            end else if (tx_word >= 16'h0030) begin
+                // Prepare last word
+                if (tx_word == 16'h05e8) begin
+                    tx_tlast <= 1;
                 end
 
-                tx_tvalid <= 1;
+                // Handle sample processing
+                case (tx_word[1:0])
+                    2'b01: begin
+                        if (new_sample) begin
+                            iq_buffer <= iq_buffer_p1;
+                            new_sample <= 0;
 
-                if (tx_tready) begin
-                    if (tx_tvalid) begin
-                        tx_word <= tx_word + 1;
+                            if (waited) begin
+                                tx_tvalid <= 0;
+                                waited <= 0;
+                                tx_word <= tx_word + 1;
+                            end
+                        end else begin
+                            tx_tvalid <= 0;
+                            if (~new_sample_last) begin
+                                if (tx_tready) begin
+                                    tx_word <= tx_word;
+                                    waited <= 1;
+                                end
+                            end else begin
+                                // tx_word <= tx_word + 1;
+                            end
+                        end
                     end
 
-                    case (tx_word)
-                        16'h0000: begin
-                            if (packet_counter == 64'b0) begin
-                                // Prime IQdata at startup
-                                IQdata <= lvds_tdata;
-                                lvds_tready <= 1;
-                            end
+                    2'b10: begin
+                        if (~new_sample && ~lvds_tready) begin
+                            lvds_tready <= 1;
                         end
-                        default: begin
-                            if ((tx_word > 16'h0031) && (tx_word[1:0] == 2'b01)) begin
-                                IQdata <= lvds_tdata;
-                                lvds_tready <= 1;
-                            end
-
-                            if (tx_word == 16'h05e8) begin
-                                tx_tlast <= 1;
-                            end
-                        end
-                        16'h05e9: begin // Last
-                            IQready <= 0;
-                            tx_tlast <= 0;
-                            tx_tvalid <= 0;
-                            tx_word <= 0;
-                            packet_counter <= packet_counter + 1;
-                            wait_counter <= 16;
-                        end
-                    endcase
-                end
+                    end
+                endcase
             end
         end
     end
--- tb/Packetizer/test_Packetizer.py
+++ tb/Packetizer/test_Packetizer.py
@@ -1,36 +1,72 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, Edge, Timer
-from cocotb.binary import BinaryValue
+from cocotb.triggers import RisingEdge, FallingEdge, Edge, Timer, Event
+# from cocotb.binary import BinaryValue
 from cocotbext import eth, axi
 
-from scapy.layers.l2 import Ether
-from scapy.layers.inet import UDP, IP
+from scapy.all import Ether, IP, UDP
 
 import random
-import numpy as np
-import zlib
+# import numpy as np
+# import zlib
+import itertools
+
 
 AXI_CLOCK_PERIOD_NS = 20
+PACKETS = 16
+TIMEOUT_LEN = 20000
+
+
+def randbytes(n, bits=8):
+    for _ in range(n):
+        yield random.getrandbits(bits)
+
+
+def randIQ(n, bits=13):
+    """
+    Returns list contains 32 bit ints in the same format as the AT86RF215
+
+    n is number of samples, each sample contains I and Q data
+    """
+    for _ in range(n):
+        iq = list(randbytes(2, bits))
+        yield (0b10 << 30) | (iq[0] << 17) | (0b01 << 14) | (iq[1] << 1), iq
+
+
+def twos_comp(val, bits):  # From https://stackoverflow.com/a/9147327
+    if (val & (1 << (bits - 1))) != 0:
+        val = val - (1 << bits)
+    return val
 
 
 class TB(object):
-    def __init__(self, dut, axi_period):
+    def __init__(self, dut, axi_period, delays=0):
         self.dut = dut
         self.log = dut._log
+        self.timeout = Event()
+
+        cocotb.fork(self.timeout_wait(self.timeout))
 
         cocotb.fork(Clock(self.dut.clk, axi_period, units="ns").start())
-        self.axi = axi.AxiStreamSink(
+        self.axi_out = axi.AxiStreamSink(
             axi.AxiStreamBus.from_prefix(dut, "tx"), dut.clk, dut.rst)
+        self.axi_in = axi.AxiStreamSource(
+            axi.AxiStreamBus.from_prefix(dut, "lvds"), dut.clk, dut.rst)
+
+        if delays == 1:
+            self.axi_in.set_pause_generator(itertools.cycle([0, 1, 1, 1, 1, 1, 1, 1]))
+        elif delays == 2:
+            self.axi_out.set_pause_generator(itertools.cycle([0, 1, 1, 1, 1, 1, 1, 1]))
 
     async def axi_recv(self) -> axi.AxiStreamFrame:
-        result = await self.axi.recv()
+        result = await self.axi_out.recv()
         return result
+
+    def lvds_send(self, data):
+        self.axi_in.send_nowait(axi.AxiStreamFrame(tdata=data))
 
     async def cycle_reset(self, wait_len=3):
         self.dut.rst.setimmediatevalue(0)
-        await RisingEdge(self.dut.clk)
-        await RisingEdge(self.dut.clk)
         await RisingEdge(self.dut.clk)
         self.dut.rst <= 1
 
@@ -38,27 +74,47 @@ class TB(object):
             await RisingEdge(self.dut.clk)
 
         self.dut.rst <= 0
-        await RisingEdge(self.dut.clk)
-        await RisingEdge(self.dut.clk)
-        await RisingEdge(self.dut.clk)
+        # await RisingEdge(self.dut.clk)
+        # await RisingEdge(self.dut.clk)
+        # await RisingEdge(self.dut.clk)
+
+    async def timeout_wait(self, event: Event):
+        timer = TIMEOUT_LEN
+
+        while True:
+            await RisingEdge(self.dut.clk)
+
+            timer -= 1
+
+            if event.is_set():
+                timer = TIMEOUT_LEN
+                event.clear()
+
+            assert timer > 0, "Timeout"
 
 
-@cocotb.test()
-async def sequential_data_test(dut):
-    tb = TB(dut, AXI_CLOCK_PERIOD_NS)
+async def sequential_data_test(dut, delays):
+    random.seed("D$3EFjTy4Wmpop#u") # For repeatability
+    tb = TB(dut, AXI_CLOCK_PERIOD_NS, delays)
     tb.log.info("Running test")
-
-    dut.lvds_tdata <= 0x8c63436c
-    dut.lvds_tvalid <= 1
 
     dut.tx_a_full <= 0
     dut.tx_a_empty <= 0
 
-    tb.axi.pause = True
     await tb.cycle_reset()
-    tb.axi.pause = False
 
-    for i in range(4):
+    for i in range(PACKETS):
+        tb.timeout.set()
+        tb.log.info(f"Sending packet {i}")
+
+        formatted_data = []
+        raw_data = []
+        for x in randIQ(366):
+            formatted_data.append(x[0])
+            raw_data.append(x[1])
+
+        tb.lvds_send(formatted_data)
+
         axi_pkt = await tb.axi_recv()
         data = axi_pkt.tdata
         pkt = Ether(data)
@@ -71,4 +127,31 @@ async def sequential_data_test(dut):
         assert pkt[UDP].len == 1480, f"{pkt[Ether].type}"
         assert len(data) == 1514
 
+        iq_data = bytes(pkt[UDP].payload)[8:]
+
+        for j in range(366):
+            sample = iq_data[j*4:(j+1)*4]
+            i_raw = int(sample[0] | sample[1] << 8)
+            q_raw = int(sample[2] | sample[3] << 8)
+            i_data = twos_comp(i_raw, 16)
+            q_data = twos_comp(q_raw, 16)
+
+            try:
+                assert i_data == twos_comp(raw_data[j][0], 13), f"Packet {i}, sample {j}"
+                assert q_data == twos_comp(raw_data[j][1], 13), f"Packet {i}, sample {j}"
+            except AssertionError as e:
+                raise e
+
     dut._log.info("Done test")
+
+@cocotb.test()
+async def no_bottleneck_test(dut):
+    await sequential_data_test(dut, 0)
+
+@cocotb.test()
+async def input_bottleneck_test(dut):
+    await sequential_data_test(dut, 1)
+
+# @cocotb.test() # Descoped output bottleneck functionality for now
+async def output_bottleneck_test(dut):
+    await sequential_data_test(dut, 2)
